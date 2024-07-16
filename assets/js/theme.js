0a156e416b27b09c5f6d73a209d905162354ab60
Adam Todorovic <a.tod95@hotmail.com>
2024-07-16 19:56:51 +1000

    comments for js theme

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,17 +1,24 @@
-// Read from LocalStorage currently set theme - light or dark mode
+// Loads the html document before the Javascript file is loaded to prevent errors
 document.addEventListener('DOMContentLoaded', () => {
+    
+    // Decalring the switch theme button in the html document
+    // If there is no switchThemeBtn the code does not execute
     const switchThemeBtn = document.getElementById('switchThemeBtn');
     if (!switchThemeBtn) return;
 
+    // Selecting the html element from the document
     const htmlElement = document.documentElement;
 
+    //  Switch theme button event listener when clicked
     switchThemeBtn.addEventListener('click', () => {
-        const newTheme = htmlElement.getAttribute('data-theme') === 'dark' ? 'light' : 'dark';
-        htmlElement.setAttribute('data-theme', newTheme);
-        localStorage.setItem('theme', newTheme);
+        const newTheme = htmlElement.getAttribute('data-theme') === 'dark' ? 'light' : 'dark'; // Toggle theme between light and dark.
+        htmlElement.setAttribute('data-theme', newTheme); // Setting  the new theme to the data-theme in the html file
     });
 
-    const savedTheme = localStorage.getItem('theme') || 'dark';
-    htmlElement.setAttribute('data-theme', savedTheme);
+    const savedTheme = localStorage.getItem('theme') || 'dark'; // Gets the saved theme from localStorage, default to dark mode (If there is no theme in local storage it defaults to dark)
+    htmlElement.setAttribute('data-theme', savedTheme); // Applies saved theme to the html element.
 });
 
+
+// This code enables the user to switch between light and dark mode , with the selected theme saved in local storage. 
+// So the theme can be re applied whenever the user returns to the site.
