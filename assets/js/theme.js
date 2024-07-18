bc6a6e3223b84f5f74c8235f41887650c5371b2a
Adam Todorovic <a.tod95@hotmail.com>
2024-07-18 16:33:01 +1000

    JS theme update 2.0

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,22 +1,27 @@
 // Loads the html document before the Javascript file is loaded to prevent errors
 document.addEventListener('DOMContentLoaded', () => {
     
-    // Decalring the switch theme button in the html document
+    // Declaring the switch theme button in the html document
     // If there is no switchThemeBtn the code does not execute
     const switchThemeBtn = document.getElementById('switchThemeBtn');
     if (!switchThemeBtn) return;
 
-    // Selecting the html element from the document
+    // Selecting the html element from the document.
     const htmlElement = document.documentElement;
 
+     // Function to set the theme.
+    const setTheme = (theme) => {
+        htmlElement.setAttribute('data-theme', theme); // Sets theme attribute to to html and saves it to local storage.
+        localStorage.setItem('theme', theme); // Save the theme to localStorage.
+    };
     //  Switch theme button event listener when clicked
     switchThemeBtn.addEventListener('click', () => {
         const newTheme = htmlElement.getAttribute('data-theme') === 'dark' ? 'light' : 'dark'; // Toggle theme between light and dark.
-        htmlElement.setAttribute('data-theme', newTheme); // Setting  the new theme to the data-theme in the html file
+        setTheme(newTheme); // Set the new theme and save it to localStorage.
     });
 
     const savedTheme = localStorage.getItem('theme') || 'dark'; // Gets the saved theme from localStorage, default to dark mode (If there is no theme in local storage it defaults to dark)
-    htmlElement.setAttribute('data-theme', savedTheme); // Applies saved theme to the html element.
+    setTheme(savedTheme); // Apply the saved theme to the html element.
 });
 
 
