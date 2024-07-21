3162dd77dc5fc481aed64361eb8d17f355dfa59c
shanwc1972 <166612646+shanwc1972@users.noreply.github.com>
2024-07-21 14:20:37 +1000

    delay-by-one bug fixed

--- assets/js/speech_v2.js
+++ assets/js/speech_v2.js
@@ -44,32 +44,11 @@ function storeOptionSettings(objOptions) {
 function read() {
     // Pass this on to Speech API
     let text = ''; // Empty string by default
-    let lang = 'en'; // English by default
     let response = {};
 
     // Boolean conditional on translations
     const translateFrom = document.getElementById("TranslateFrom").value;
     const translateTo = document.getElementById("TranslateTo").value;
-    const translateInput = !((translateFrom === "en") && (translateTo === "en"));
-
-    // Grab user input
-    if(translateInput === false) {
-        // response.language = 'en';
-        // response.output = userInput.value;
-        text = userInput.value;
-        console.log("Will not translate!");
-    }
-
-    if(translateInput === true) {
-        console.log("Translating..");
-
-        // Call our translate function
-        response = translate(userInput.value, translateFrom, translateTo); // Returns object
-        console.log(response);
-
-        text = response.output;
-        lang = response.language;
-    }
 
     // Audio Output options code here
     let objOptions = getOptionSettings();
@@ -80,74 +59,72 @@ function read() {
     objOptions.pitch = Number(selectPitch.value);
     objOptions.rate = Number(selectRate.value / 10);
 
+
+    // Male or Female conditionals
+    if(Voice.value === "Male") {
+        switch(translateTo) {
+            case "en":
+                objOptions.voice = "Australian Male";
+                break;
+            case "fr":
+                objOptions.voice = "French Male";
+                break;
+            case "de":
+                objOptions.voice = "Deutsch Male";
+                break;
+            case "ja":
+                objOptions.voice = "Japanese Male";
+                break;
+            case "ar":
+                objOptions.voice = "Arabic Male";
+                break;
+        }
+    }
+    else {
+        switch(translateTo) {
+            case "en":
+                objOptions.voice = "UK English Female";
+                break;
+            case "fr":
+                objOptions.voice = "French Female";
+                break;
+            case "ja":
+                objOptions.voice = "Japanese Female";
+                break;
+            case "de":
+                objOptions.voice = "Deutsch Female";
+                break;
+            case "ar":
+                objOptions.voice = "Arabic Female";
+                break;
+        }
+    }
+    
+    //responsiveVoice.speak(text,voice, objResponsiveParameters);
+    
+    console.log(objOptions);
     // Pass to API
     switch(API.value) {
         case "responsiveVoice":
-            responsiveVoiceSpeak(text,lang, objOptions);
+            //responsiveVoiceSpeak(text,lang, objOptions);
+            responsivetranslateandspeak(userInput.value, translateFrom, translateTo, objOptions)
             break;
         case "synthSpeech":
             console.log("coming soon!");
             break;
     }
-
-    console.log(text);
+    //save our voice settings
+    storeOptionSettings(objOptions);
 }
 
-function responsiveVoiceSpeak(text,lang, objOptions) {
+function responsiveVoiceSpeak(text, voice, objOptions) {
 
-    let voice = "US English Male"; // By default
     let objResponsiveParameters = {};
-
-    // Male or Female conditionals
-    if(Voice.value === "Male") {
-        switch(lang) {
-            case "en":
-                voice = "Australian Male";
-                break;
-            case "fr":
-                voice = "French Male";
-                break;
-            case "de":
-                voice = "Deutsch Male";
-                break;
-            case "ja":
-                voice = "Japanese Male";
-                break;
-            case "ar":
-                voice = "Arabic Male";
-                break;
-        }
-    }
-    else {
-        switch(lang) {
-            case "en":
-                voice = "UK English Female";
-                break;
-            case "fr":
-                voice = "French Female";
-                break;
-            case "ja":
-                voice = "Japanese Female";
-                break;
-            case "de":
-                voice = "Deutsch Female";
-                break;
-            case "ar":
-                voice = "Arabic Female";
-                break;
-        }
-    }
-
-    // Now, you shall speak!
-    console.log(voice);
-    
     objResponsiveParameters.pitch = objOptions.pitch;
     objResponsiveParameters.rate = objOptions.rate;
     objResponsiveParameters.volume = objOptions.volume;
-    responsiveVoice.speak(text,voice, objResponsiveParameters);
-
-    //save our voice settings
-    storeOptionSettings(objOptions);
+    //Speakout translated text
+    responsiveVoice.speak(text, voice, objResponsiveParameters);
 }
 
 // Button onClick event listener
--- assets/js/translate.js
+++ assets/js/translate.js
@@ -3,24 +3,25 @@ const API_KEY = 'AIzaSyCMJ05_dwlQH6ipqi-7lOuxRcSQYw2-n2Q';
 let result = {}; // Create JS object
 let url = '';
 
-function translate(myText, sourceLang, targetLang) {
+//function translate(myText, sourceLang, targetLang, objOptions) {
     // Grabs user input and generates API URL
-    url = `https://translation.googleapis.com/language/translate/v2?key=${API_KEY}&source=${sourceLang}&target=${targetLang}&q=${myText}`;
+//    url = `https://translation.googleapis.com/language/translate/v2?key=${API_KEY}&source=${sourceLang}&target=${targetLang}&q=${myText}`;
 
     // Add key: value to our object
-    result.language = targetLang;
+//    result.language = targetLang;
 
     // Fetch from Google API
-    fetchData(url);
+//    fetchData(url, objOptions);
 
     // Return object
-    return result;
-}
+//    return result;
+//}
 
-async function fetchData(url) {
+async function fetchData(url, objOptions) {
     try {
         const response = await fetch(url);
         const data = await response.json();
+        
         const translatedText = data.data.translations[0].translatedText;
 
         // Update UI output
@@ -29,10 +30,26 @@ async function fetchData(url) {
 
         // Add key: value to our object
         result.output = translatedText;
+        // Now, call our text to speech API
+        responsiveVoiceSpeak(translatedText, objOptions.voice, objOptions);
 
     } catch (error) {
         console.error('Error fetching data:', error);
     }
+}
 
+function responsivetranslateandspeak(myText, sourceLang, targetLang, objOptions) {
+    const outputEl = document.getElementById("langOutput");
+    let translateresult = {};
+    const translateInput = !(sourceLang == targetLang);
+    url = `https://translation.googleapis.com/language/translate/v2?key=${API_KEY}&source=${sourceLang}&target=${targetLang}&q=${myText}`;
 
+    if(!translateInput) {
+        console.log("Will not translate!");
+        responsiveVoiceSpeak(myText, objOptions.voice, objOptions);
+    } else {
+        console.log("Translating..");
+        // Fetch from Google API
+        fetchData(url, objOptions);
+    }
 }
